8320b0377e5e2a1eefe56ae2f2188a3d97152ce0
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2022-02-17 03:57:55 -0300

    feate: include user register

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: "User's email",
+  })
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8, {
+    message: 'Senha muito curta',
+  })
+  @ApiProperty({
+    description: "User's password",
+  })
+  password: string;
+}
--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true, autoCreate: true })
+export class User extends Document {
+  @Prop({ type: String, required: true, unique: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
